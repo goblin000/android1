99c7e1f2131562579426a1cbe6e0a8877dba9213
goblin000 <sz_prog@mosgt.ru>
2020-10-23 01:38:30 +0300

    Урок 6.

---
+++ Lesson_6/app/src/main/java/com/example/lesson1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.lesson1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
+
+
